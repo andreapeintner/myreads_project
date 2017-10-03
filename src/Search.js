5294e40b218bae5a02c3dfbb86963b2e1b59c4e2
andi <andreapeintner@gmail.com>
2017-10-03 12:54:31 +0200

    remove state
    call books with props,
    move updateQuery to App Component

--- src/Search.js
+++ src/Search.js
@@ -1,38 +1,16 @@
 import React, { Component } from 'react'
-import * as BooksAPI from './BooksAPI'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Book from './Book'
 
 class Search extends Component {
   static propTypes = {
-      booksOnShelf: PropTypes.array.isRequired,
+      books: PropTypes.array.isRequired,
       changeShelf: PropTypes.func.isRequired
     }
-    state = {
-      query: '',
-      books: []
-    }
-
     updateQuery = (query) => {
-      if (!query) {
-        this.setState({query: '', books: []})
-      } else {
-        this.setState({ query: query.trim() })
-        BooksAPI.search(query).then((books) => {
-          if(books.length){
-              books.forEach((book, index) => {
-                  let displayedBooks = this.state.books.find((b) => b.id === book.id)
-                  book.shelf = displayedBooks ? displayedBooks.shelf : 'none'
-              })
-          }
-          if (books.error) {
-            books = []
-          }
-          this.setState({books})
-        })
-      }
-    }
+        this.props.updateQuery(query.trim());
+    };
     render () {
       return (
         <div className="search-books">
@@ -50,14 +28,14 @@ class Search extends Component {
             <ol className="books-grid">
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  {this.state.books.map(book => (
+                  {this.props.books.map((book) => (
+                    <li key={book.id} className="contact-list-item">
                       <Book
-                        changeShelf={this.props.changeShelf}
-                        key={book.id}
                         book={book}
+                        changeShelf={this.props.changeShelf}
                       />
-                    ))
-                  }
+                    </li>
+                  ))}
                 </ol>
               </div>
             </ol>
